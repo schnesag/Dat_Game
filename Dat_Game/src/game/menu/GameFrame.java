988616b6455cf2d0f8952f1fc124024c1b3d3a07
Seth Schneider <sschneider496@gmail.com>
2016-05-12 10:22:26 -0500

    The Standard GAME MENU FOOL

--- Dat_Game/src/game/menu/GameFrame.java
+++ Dat_Game/src/game/menu/GameFrame.java
@@ -1,18 +1,50 @@
 package game.menu;
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 public class GameFrame extends JFrame
 {
+	JButton aButton;
+	JButton anotherButton;
 	
-	public GameFrame(){
-	super();
-	this.setSize(500,500);
-	JButton aButton = new JButton("This is a button");
-	JButton anotherButton = new JButton("This is another button");
-	this.getContentPane().add(aButton, BorderLayout.CENTER);
-	this.getContentPane().add(anotherButton, BorderLayout.PAGE_START);
+		public GameFrame()
+		{
+			super();
+			this.setSize(500,500);
+			aButton = new JButton("This is a button");
+			anotherButton = new JButton("This is another button");
+			this.getContentPane().add(aButton, BorderLayout.CENTER);
+			this.getContentPane().add(anotherButton, BorderLayout.PAGE_START);
+			addListeners();
+		}
 	
+	// Method to add listeners to the buttons.
+	private void addListeners()
+	{
+		aButton.addActionListener( new aButtonListener());
+		anotherButton.addActionListener(new anotherButtonListener());
 	}
+	
+	//********* Adding Listeners ************
+	private class aButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("You pressed a Button!");
+			
+		}
+
+	}
+	
+	private class anotherButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("You pressed Another Button!");
+		}
+	}
+
 	
 }
