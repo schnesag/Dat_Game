aef794d34ec71171595f1267b5af0379f1331339
will <spurrw@uni.edu>
2016-05-13 08:43:30 -0500

    slightly modified Run.java
    Changed GameFrame.java to my base class

--- Dat_Game/src/GameFrame.java
+++ Dat_Game/src/GameFrame.java
@@ -1,63 +1,56 @@
-
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
+// ! can pass 'this' to a method and use it later ( like 'self' )
 
-//     Pull This Change
+public class GameFrame extends JFrame {
 
-public class GameFrame extends JFrame
-{
-	JButton aButton;
-	JButton anotherButton;
-	JButton anotherAnotherButton;
-		public GameFrame()
-		{
-			super();
-			this.setSize(500,500);
-			aButton = new JButton("This is a button");
-			anotherButton = new JButton("This is another button");
-			anotherAnotherButton = new JButton("This is another another button");
-			this.getContentPane().add(aButton, BorderLayout.CENTER);
-			this.getContentPane().add(anotherButton, BorderLayout.PAGE_START);
-			this.getContentPane().add(anotherAnotherButton, BorderLayout.PAGE_END);
-			addListeners();
-		}
+	int width, height;
 	
-	// Method to add listeners to the buttons.
-	private void addListeners()
-	{
-		aButton.addActionListener( new aButtonListener());
-		anotherButton.addActionListener(new anotherButtonListener());
-		anotherAnotherButton.addActionListener(new anotherAnotherButtonListener());
+	Ship ship;
+	
+	JButton left, right;
+	
+	public GameFrame (String _title, int _width, int _height) {
+		super(_title); // sets title
+		
+		width = _width; // saves width and height
+		height = _height;
+		
+		this.setSize(width, height); // sets width and height
+		this.setLayout(null); // free layout for x and y positioning
+		this.setVisible(true);
+		
+		// create ship and add to JFrame this
+		ship = new Ship(20, 20, 80, 80);
+		this.add(ship);
+		
+		// move ship right
+		right = new JButton("-->");
+		right.addActionListener(new moveButtonListener());
+		right.setBounds(80, 200, 50, 15);
+		this.add(right);
+		
+		// move ship left
+		left = new JButton("<--");
+		left.addActionListener(new moveButtonListener());
+		left.setBounds(20, 200, 50, 15);
+		this.add(left);
 	}
-	
-	//********* Adding Listeners ************
-	private class aButtonListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			System.out.println("You pressed a Button!");
+
+	private class moveButtonListener implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
 			
-		}
-
-	}
-	
-	private class anotherButtonListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			System.out.println("You pressed Another Button!");
-		}
-	}
-	private class anotherAnotherButtonListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			System.out.println("You pressed Another Another Button!");
+			if (event.getSource().equals(right)) // if right button is source
+				ship.increaseX(); // move right
+			
+			else if (event.getSource().equals(left)) // if left button is source
+				ship.decreaseX(); // move left
+			
+			else // neither right or left button called
+				System.out.println("moveButtonListener: no button matching event.getSource()");
 		}
 	}
-
 	
 }
---
+++ Dat_Game/src/Run.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		GameFrame game = new GameFrame("Seth and Will's Game", 500, 500);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setVisible(true);
+
+	}
+
+}
